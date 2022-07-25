f7376ab083e70301910ee17d63deba77d59ab861
OGUGUA54 <nndukaobikwelu@gmail.com>
2022-07-25 18:26:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+// Add your code here
+let firstname= "ogugua"
+let lastname= "Ndukaobikwelu"
+let fullname= firstname+lastname
+let introduction= "hello, my name is " + fullname
+let myEyesColor= "black"
+ let myHairColor= "black"
+ let myNumberOfSiblings= 1
+ let myFavouriteFood= "plantain"
+ let myFavouriteColor= "red"
+ console.log(firstname)
+ console.log(lastname)
+ console.log(fullname)
+console.log(introduction + fullname)
+console.log( myEyesColor)
+console.log(myHairColor)
+console.log(myNumberOfSiblings)
+console.log(myFavouriteFood)
+console.log(myFavouriteColor)
+
+
+// Don't edit or touch the code below here!
+// Don't edit or touch the code below here!
+// Don't edit the code below here!
+
+let h1 = document.querySelector('h1')
+h1.textContent = 'Congrats! You have linked your JavaScript file and it is now working'
+
+let paragraph1 = document.createElement('p');
+paragraph1.setAttribute('id', 'introduction')
+paragraph1.textContent = introduction;
+
+let paragraph2 = document.createElement('p');
+paragraph2.setAttribute('id', 'my-hair-color')
+paragraph2.textContent = `My eye color is ${myEyesColor} and my hair color is ${myHairColor}.`;
+
+let paragraph3 = document.createElement('p');
+paragraph3.setAttribute('id', 'my-favourite-color')
+paragraph3.textContent = 'my favorite color is ' + myFavouriteColor;
+
+let paragraph4 = document.createElement('p');
+paragraph4.setAttribute('id', 'my-favourite-food')
+paragraph4.textContent = 'my favorite food is ' + myFavouriteFood;
+
+let paragraph5 = document.createElement('p');
+paragraph5.setAttribute('id', 'my-number-of-siblings')
+paragraph5.textContent = 'I have ' + myNumberOfSiblings + ' siblings.';
+
+let section = document.querySelector('section')
+section.appendChild(paragraph1);
+section.appendChild(paragraph2);
+section.appendChild(paragraph3);
+section.appendChild(paragraph4);
+section.appendChild(paragraph5);
+
